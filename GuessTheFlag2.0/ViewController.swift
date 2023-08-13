7f18f9d33cbb80e962ea47733b13c6bacadaa7bd
Linconl Rufino <linconldasilvarufino@gmail.com>
2023-08-13 19:31:29 -0300

    Initial Commit

---
+++ GuessTheFlag2.0/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GuessTheFlag2.0
+//
+//  Created by Linconl Rufino on 13/08/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
