ad5dbf85c809f2dad192bfaf6b7946e4fcf1bd3e
Linconl Rufino <linconldasilvarufino@gmail.com>
2023-08-21 22:46:19 -0300

    #some labels and UIColor extension

--- GuessTheFlag2.0/ViewController.swift
+++ GuessTheFlag2.0/ViewController.swift
@@ -8,38 +8,46 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     private lazy var titleLabel : UILabel = {
         let titleLabel = UILabel()
-        titleLabel.text = "Titulo Teste"
-        titleLabel.textColor = .red
+        titleLabel.text = "Country Selected"
+        titleLabel.textColor = .black
         titleLabel.translatesAutoresizingMaskIntoConstraints = false
         titleLabel.font = UIFont.systemFont(ofSize: 24)
         
         return titleLabel
     }()
     
+    private lazy var scoreLabel : UILabel = {
+        let scoreLabel = UILabel()
+        scoreLabel.translatesAutoresizingMaskIntoConstraints = false
+        scoreLabel.text = "Score"
+        scoreLabel.textColor = .black
+        return scoreLabel
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-
         setupLayout()
     }
     
     func setupLayout(){
         view = UIView()
+        view.backgroundColor = .corDeFundoTela
+        
         view.addSubview(titleLabel)
+        view.addSubview(scoreLabel)
         
         NSLayoutConstraint.activate([
+            titleLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            titleLabel.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor),
             
-            titleLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor)
-        
+            scoreLabel.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -100),
+            scoreLabel.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor)
         ])
         
     }
-    
-
 
 }
 
